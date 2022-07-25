1e15bf99d96893cd0feb03bd667cabefbe1d329d
Samir Karki <karkisamir99@gmail.com>
2022-07-25 23:33:18 +0545

    All files done

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/all.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Beau+Rivage&family=Moo+Lah+Lah&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <div class="container-fluid bg-dark">
+        <div class="row">
+            <div class="col-sm-2">
+                @include('include.sidebar')
+            </div>
+            <div class="col-sm-10">
+                {{$content}}
+            </div>
+        </div>
+    </div>
+
+<script src="{{asset('js/all.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundel.js')}}"></script>
+</body>
+</html>
